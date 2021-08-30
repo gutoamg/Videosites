ff48778bc8a0bf7e2f0465c71ffca8cf8bda65bb
Augusto M. Gouveia <a166949@dac.unicamp.br>
2021-08-30 10:03:00 -0300

    Header meta tag update

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,3 @@
-import favIcon from '../public/gutoamg-Recording-Favicon-32x32.png';
-import VideositeImg from '../public/Videosite_screenshot.png';
 import Head from 'next/head';
 
 const Header = () => {
@@ -12,19 +10,20 @@ const Header = () => {
                 name="description"    
                 content={'This is my portfolio and a Videosite, a concept Design that presents a different way of navigating through websites.'}
             />
-            <meta property="og:image" content={VideositeImg} />
-            <meta property="og:title" content="Videosites" />
-            <meta
-                property="og:description"
-                content={'This is my portfolio and a Videosite, a concept Design that presents a different way of navigating through websites.'}
-            />
-            <meta property="twitter:image" content={VideositeImg} />
-            <meta property="twitter:title" content="Videosites" />
-            <meta
-                property="twitter:description"
-                content={'This is my portfolio and a Videosite, a concept Design that presents a different way of navigating through websites.'}
-            />
 
+            // Twitter
+            <meta name="twitter:card" content='This is my portfolio and a Videosite, a concept Design that presents a different way of navigating through websites.' key="twcard" />
+            <meta name="twitter:creator" content={'Augusto M. Gouveia'} key="twhandle" />
+
+            //Open Graph
+            <meta property="og:url" content={'https://gutoamg.com/'} key="ogurl" />
+            <meta property="og:image" content='public/Videosite_screenshot.png' key="ogimage" />
+            <meta property="og:site_name" content={'gutoamg'} key="ogsitename" />
+            <meta property="og:title" content={Videosites} key="ogtitle" />
+            <meta property="og:description" content={'This is my portfolio and a Videosite, a concept Design that presents a different way of navigating through websites.'} key="ogdesc" />
+
+
+            // Favicons
             <link rel="icon" type="image/png" sizes="32x32" href={"/gutoamg-Recording-Favicon-32x32.png"}/>
             <link rel="icon" type="image/png" sizes="16x16" href={"/gutoamg-Recording-Favicon-16x16.png"}/>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
@@ -32,7 +31,10 @@ const Header = () => {
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
             <meta name="theme-color" content="rgb(24,24,24)"/>
 
+            // Css file
             <link rel="stylesheet" type="text/css" href={"../styles/newSite.css"}/>
+
+            // Fonts
             <link rel="preconnect" href={"https://fonts.gstatic.com"}/>
             <link href={"https://fonts.googleapis.com/css2?family=Saira:wght@500&display=swap"} rel="stylesheet"/> 
             <link href={"https://fonts.googleapis.com/css2?family=Saira:wght@300&display=swap"} rel="stylesheet"/> 
